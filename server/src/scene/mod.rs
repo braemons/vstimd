c5481d564f34b26c4a917cc94f8df03f1b397b7b
Joscha Schmiedt <schmiedt@uni-bremen.de>
2026-06-30 13:03:27 +0200

    Rename state.rs -> scene_state.rs

--- server/src/scene/mod.rs
+++ server/src/scene/mod.rs
@@ -3,14 +3,14 @@ pub mod command;
 pub mod deferred;
 pub mod photodiode;
 pub mod scene_config;
-mod state;
+mod scene_state;
 pub mod stimulus;
 
 pub use animation::{AnimState, AnimationEntry, Edge, FinalAction, VtlBit};
 pub use deferred::Deferred;
 pub use photodiode::PhotoDiodeState;
 pub use scene_config::{LoadMode, SceneConfig};
-pub use state::{SceneRuntimeState, SceneState};
+pub use scene_state::{SceneRuntimeState, SceneState};
 pub use stimulus::{
     Anchor, CircleStimulus, DrawMode, EllipseStimulus, GratingMask, GratingParams, GratingStimulus,
     LanguageStyle, RectStimulus, ShapeAppearance, ShapeCommon, Stimulus, StimulusEntry,
